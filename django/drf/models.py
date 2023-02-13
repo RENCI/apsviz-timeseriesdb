bbb08007c36a90d4ecbc13b0510e6c4d4c5e286e
James McManus <jmpmcman@renci.org>
2023-02-13 10:30:53 -0500

    Added new variables

--- django/drf/models.py
+++ django/drf/models.py
@@ -14,6 +14,7 @@ class source_meta(models.Model):
     data_source = models.TextField(30,null=False)
     source_name = models.TextField(20,null=False)
     source_archive = models.TextField(15,null=False)
+    source_variable = models.TextField(15,null=False)
     location_type = models.TextField(10,null=False)
 
 # Model for archiving the harvesting gauge data files meta-data
@@ -43,7 +44,11 @@ class gauge_station(models.Model):
     country = models.TextField(20,null=True)
     state = models.TextField(20,null=True)
     county = models.TextField(20,null=True)
+    apsviz_station = models.BooleanField(null=False)
     geom = models.PointField(null=False)
+
+    class Meta:
+        indexes = [models.Index(fields=['station_id', 'station_name', 'geom']),]
 
 # Model for gauge source data, including ADCIRC data 
 class gauge_source(models.Model):
@@ -54,6 +59,9 @@ class gauge_source(models.Model):
     source_archive = models.TextField(20,null=False) # (nomads?, contrails, renci, tacc..?)
     units = models.TextField(10,null=True)
 
+    class Meta:
+        indexes = [models.Index(fields=['source_id', 'station_id', 'data_source']),]
+
 # Model for data data downloaded by harvest scripts
 class gauge_data(models.Model):
     obs_id = models.AutoField(primary_key=True)
@@ -61,8 +69,13 @@ class gauge_data(models.Model):
     timemark = models.DateTimeField(null=False)
     time =  TimescaleDateTimeField(interval="10 day")
     water_level = models.FloatField(null=True)
+    wave_height = models.FloatField(null=True)
     wind_speed = models.FloatField(null=True)
     air_pressure = models.FloatField(null=True)
+    flow_volume = models.FloatField(null=True)
+
+    class Meta:
+        indexes = [models.Index(fields=['obs_id', 'source_id', 'water_level', 'timemark']),]
  
 # Model for combined view of gauge_station, gauge_source and gauge_data
 class gauge_station_source_data(models.Model):
@@ -73,8 +86,10 @@ class gauge_station_source_data(models.Model):
     timemark = models.DateTimeField(null=True)
     time =  TimescaleDateTimeField(interval="10 day")
     water_level = models.FloatField(null=True)
+    wave_height = models.FloatField(null=True)    
     wind_speed = models.FloatField(null=True)
     air_pressure = models.FloatField(null=True)
+    flow_volume = models.FloatField(null=True)    
     tz = models.TextField(8,null=False)
     units = models.TextField(10,null=True)
     gauge_owner = models.TextField(200,null=False) # (noaa, ncem, usgs...)
@@ -83,6 +98,7 @@ class gauge_station_source_data(models.Model):
     source_archive = models.TextField(20,null=False) # (nomads?, contrails, renci, tacc..?)
     location_name = models.TextField(200,null=False) # (name from noaa and contrails)
     location_type = models.TextField(8,null=False)
+    apsviz_station = models.BooleanField(null=False)
     country = models.TextField(20,null=True)
     state = models.TextField(20,null=True)
     county = models.TextField(20,null=True)
