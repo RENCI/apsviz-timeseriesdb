79e75471f70a88c4041333b1b553b729ccb81900
James McManus <jmpmcman@renci.org>
2024-02-13 12:17:00 -0500

    Added model split changes

--- django/drf/models.py
+++ django/drf/models.py
@@ -9,19 +9,30 @@ STATUS_CHOICES = (
     ('p', 'Published')
 )
 
-# Model for source meta, which is used as input for scripts
-class source_meta(models.Model):
+# Model for source observation meta, which is used as input for scripts
+class source_obs_meta(models.Model):
     data_source = models.TextField(200,null=False)
     source_name = models.TextField(20,null=False)
     source_archive = models.TextField(15,null=False)
     source_variable = models.TextField(15,null=False)
     filename_prefix = models.TextField(100,null=False)
     location_type = models.TextField(10,null=False)
-    data_type = models.TextField(6,null=False)
     units = models.TextField(10,null=True)
 
-# Model for archiving the harvesting gauge data files meta-data
-class harvest_data_file_meta(models.Model):
+# Model for source ADCIRC model meta, which is used as input for scripts
+class source_model_meta(models.Model):
+    data_source = models.TextField(200,null=False)
+    source_name = models.TextField(20,null=False)
+    source_archive = models.TextField(15,null=False)
+    source_variable = models.TextField(15,null=False)
+    source_instance = models.TextField(25,null=False)
+    forcing_metclass = models.TextField(15,null=False)
+    filename_prefix = models.TextField(100,null=False)
+    location_type = models.TextField(10,null=False)
+    units = models.TextField(10,null=True)
+
+# Model for archiving the harvesting gauge observation data files meta-data
+class harvest_obs_file_meta(models.Model):
     file_id = models.AutoField(primary_key=True)
     dir_path = models.TextField(100,null=False)
     file_name = models.TextField(100,null=False)
@@ -31,7 +42,30 @@ class harvest_data_file_meta(models.Model):
     data_source = models.TextField(200,null=False)
     source_name = models.TextField(30,null=False)
     source_archive = models.TextField(30,null=False)
+    source_variable = models.TextField(15,null=False)
+    location_type = models.TextField(10,null=False)
     timemark = models.DateTimeField(null=True)
+    processing_datetime = models.DateTimeField(null=True)
+    ingested = models.BooleanField(null=False)
+    overlap_past_file_date_time = models.BooleanField(null=False)
+
+# Model for archiving the harvesting gauge observation data files meta-data
+class harvest_model_file_meta(models.Model):
+    file_id = models.AutoField(primary_key=True)
+    dir_path = models.TextField(100,null=False)
+    file_name = models.TextField(100,null=False)
+    data_date_time = models.DateTimeField(null=True) 
+    data_begin_time = models.DateTimeField(null=True)
+    data_end_time = models.DateTimeField(null=True)
+    data_source = models.TextField(200,null=False)
+    source_name = models.TextField(30,null=False)
+    source_archive = models.TextField(30,null=False)
+    source_instance = models.TextField(25,null=False)
+    forcing_metclass = models.TextField(15,null=False)
+    advisory = models.TextField(40,null=True)
+    model_run_id = models.TextField(40,null=True)
+    timemark = models.DateTimeField(null=True)
+    processing_datetime = models.DateTimeField(null=True)
     ingested = models.BooleanField(null=False)
     overlap_past_file_date_time = models.BooleanField(null=False)
 
@@ -44,6 +78,8 @@ class apsviz_station_file_meta(models.Model):
     data_source = models.TextField(200,null=False)
     source_name = models.TextField(30,null=False)
     source_archive = models.TextField(30,null=False)
+    source_instance = models.TextField(25,null=False)
+    forcing_metclass = models.TextField(15,null=False)
     grid_name = models.TextField(30,null=False)
     model_run_id = models.TextField(40,null=True)
     timemark = models.DateTimeField(null=False)
@@ -101,7 +137,9 @@ class apsviz_station(models.Model):
     model_run_id = models.TextField(40,null=True)
     data_source = models.TextField(200,null=False)
     source_name = models.TextField(30,null=False)
+    source_instance = models.TextField(35,null=False)
     source_archive = models.TextField(20,null=False) # (nomads?, contrails, renci, tacc..?)
+    forcing_metclass = models.TextField(15,null=False)
     location_type = models.TextField(8,null=False)
     grid_name = models.TextField(30,null=False)
     csvurl =  models.TextField(100,null=True)
@@ -155,7 +193,6 @@ class gauge_data(models.Model):
     wave_height = models.FloatField(null=True)
     wind_speed = models.FloatField(null=True)
     air_pressure = models.FloatField(null=True)
-    flow_volume = models.FloatField(null=True)
     stream_elevation = models.FloatField(null=True)
 
     objects = models.Manager()
@@ -176,7 +213,6 @@ class gauge_station_source_data(models.Model):
     wave_height = models.FloatField(null=True)    
     wind_speed = models.FloatField(null=True)
     air_pressure = models.FloatField(null=True)
-    flow_volume = models.FloatField(null=True)
     stream_elevation = models.FloatField(null=True)
     tz = models.TextField(8,null=False)
     units = models.TextField(10,null=True)
@@ -199,3 +235,65 @@ class gauge_station_source_data(models.Model):
         managed = False
         db_table = "drf_gauge_station_source_data"
 
+# Model for ADCIRC model source data
+class model_source(models.Model):
+    source_id = models.AutoField(primary_key=True)
+    station_id = models.IntegerField()
+    data_source = models.TextField(50,null=False) # (grid names such as hsofs_0, and just gauge) THINK ABOUT TYPE!
+    source_instance = models.TextField(35,null=False)
+    source_name = models.TextField(20,null=False) # (noaa, ncem, adcirc_nowcast, adcirc_forecast?)
+    forcing_metclass = models.TextField(15,null=False)
+    source_archive = models.TextField(20,null=False) # (nomads?, contrails, renci, tacc..?)
+    units = models.TextField(10,null=True)
+
+    class Meta:
+        indexes = [models.Index(fields=['source_id', 'station_id', 'data_source']),]
+
+# Model for ADCIRC model data generated by harvest scripts
+class model_data(models.Model):
+    model_id = models.AutoField(primary_key=True)
+    source_id = models.IntegerField()
+    timemark = models.DateTimeField(null=False)
+    time =  TimescaleDateTimeField(interval="10 day")
+    water_level = models.FloatField(null=True)
+    wave_height = models.FloatField(null=True)
+
+    objects = models.Manager()
+    timescale = TimescaleManager()
+
+    class Meta:
+        indexes = [models.Index(fields=['model_id', 'source_id', 'water_level', 'timemark']),]
+
+# Model for combined view of gauge_station, gauge_source and gauge_data
+class model_station_source_data(models.Model):
+    model_id = models.IntegerField(primary_key=True)
+    source_id = models.IntegerField()
+    station_id = models.IntegerField()
+    station_name = models.TextField(20,null=False)
+    timemark = models.DateTimeField(null=True)
+    time =  TimescaleDateTimeField(interval="10 day")
+    model_run_id = models.TextField(35,null=False) # (noaa, ncem, adcirc_nowcast, adcirc_forecast?)
+    water_level = models.FloatField(null=True)
+    wave_height = models.FloatField(null=True)
+    tz = models.TextField(8,null=False)
+    units = models.TextField(10,null=True)
+    gauge_owner = models.TextField(200,null=False) # (noaa, ncem, usgs...)
+    data_source = models.TextField(200,null=False) # (grid names such as hsofs_0, and just gauge) THINK ABOUT TYPE!
+    source_name = models.TextField(20,null=False) # (noaa, ncem, adcirc_nowcast, adcirc_forecast?)
+    source_archive = models.TextField(20,null=False) # (nomads?, contrails, renci, tacc..?)
+    source_instance = models.TextField(35,null=False)
+    forcing_metclass = models.TextField(15,null=False)
+    location_name = models.TextField(200,null=False) # (name from noaa and contrails)
+    location_type = models.TextField(8,null=False)
+    apsviz_station = models.BooleanField(null=False)
+    country = models.TextField(20,null=True)
+    state = models.TextField(20,null=True)
+    county = models.TextField(20,null=True)
+    geom = models.PointField(null=False)
+
+    objects = models.Manager()
+    timescale = TimescaleManager()
+
+    class Meta:
+        managed = False
+        db_table = "drf_model_station_source_data"
--- django/drf/serializers.py
+++ django/drf/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework_gis.serializers import GeoFeatureModelSerializer
 from rest_framework.serializers import ModelSerializer
-from .models import gauge_station_source_data, gauge_station
+from .models import gauge_station_source_data, gauge_station, model_station_source_data
 from drf_queryfields import QueryFieldsMixin
 
 # Serializer, with GeoFeatureModelSerializer, for tables for the gauge_station_source_data model view.
@@ -10,7 +10,9 @@ class gauge_station_source_data_Serializer(GeoFeatureModelSerializer):
         model = gauge_station_source_data 
         geo_field = 'geom'
         id_field = 'obs_id'
-        fields = ('obs_id','source_id','station_id','station_name','timemark','time','water_level','wave_height','wind_speed','air_pressure','flow_volume','stream_elevation','tz','units','gauge_owner','data_source','source_name','source_archive','location_name','location_type','apsviz_station','country','state','county')
+        fields = ('obs_id','source_id','station_id','station_name','timemark','time','water_level','wave_height','wind_speed','air_pressure',
+                  'stream_elevation','tz','units','gauge_owner','data_source','source_name','source_archive','location_name','location_type','apsviz_station',
+                  'country','state','county')
 
 # Serializer, with QueryFieldsMixin, for tables for the gauge_station_source_data model view
 # QueryFieldsMixin enables the selection of specific fields as output, but it conflicts with 
@@ -18,7 +20,8 @@ class gauge_station_source_data_Serializer(GeoFeatureModelSerializer):
 class gauge_timemark_Serializer(QueryFieldsMixin, ModelSerializer):
     class Meta:
         model = gauge_station_source_data
-        fields = ('obs_id','source_id','station_id','station_name','timemark','time','water_level','tz','gauge_owner','data_source','source_name','source_archive','location_name','location_type','country','state','county')
+        fields = ('obs_id','source_id','station_id','station_name','timemark','time','water_level','tz','gauge_owner','data_source','source_name','source_archive',
+                  'location_name','location_type','country','state','county')
 
 class gauge_station_Serializer(GeoFeatureModelSerializer):
     class Meta:
@@ -26,3 +29,22 @@ class gauge_station_Serializer(GeoFeatureModelSerializer):
         geo_field = 'geom'
         id_field = 'station_id'
         fields = ('station_id','station_name','tz','gauge_owner','location_name','location_type','apsviz_station','country','state','county')
+
+# Serializer, with GeoFeatureModelSerializer, for tables for the gauge_station_source_data model view.
+# GeoFeatureModelSerializer enables spatial searches.
+class model_station_source_data_Serializer(GeoFeatureModelSerializer):
+    class Meta:
+        model = model_station_source_data
+        geo_field = 'geom'
+        id_field = 'model_id'
+        fields = ('model_id','source_id','station_id','station_name','timemark','time','model_run_id','water_level','wave_height','tz','units','gauge_owner','data_source',
+                  'source_name','source_instance','source_archive','forcing_metclass','location_name','location_type','apsviz_station','country','state','county')
+
+# Serializer, with QueryFieldsMixin, for tables for the gauge_station_source_data model view
+# QueryFieldsMixin enables the selection of specific fields as output, but it conflicts with
+# GeoFeatureModelSerializer so it has to have its own serializer
+class model_timemark_Serializer(QueryFieldsMixin, ModelSerializer):
+    class Meta:
+        model = model_station_source_data
+        fields = ('model_id','source_id','station_id','station_name','timemark','time','model_run_id','water_level','tz','gauge_owner','data_source','source_name',
+                  'source_instance','source_archive','forcing_metclass','location_name','location_type','country','state','county')
